35de15ec6ace4b6f06de5d0c1017ce7de39ba0a0
Dara Popova <86371720+sosasosaso@users.noreply.github.com>
2024-06-13 17:23:30 +0300

    Create Mage.java

---
+++ Mage.java
@@ -0,0 +1,14 @@
+class Mage extends Character{
+  private int intelligence;
+  private int spellPower;
+
+  public Mage (int name, int health){
+    super(name, health);
+    this.intelligence = 120;
+    this.spellPower = 200;
+  }
+public void castSpell(Character target){
+  
+}
+  
+}
