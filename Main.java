4e457a2a803f6abc6cc38f0d74edc87fbcbb8207
Dara Popova <86371720+sosasosaso@users.noreply.github.com>
2024-06-13 17:58:29 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
 import java.util.Scanner;
+
 public class Main{
   public static void main (String[] args){
     Scanner scan = new Scanner(System.in);
-    
+    Warrior warrior = new Warrior("Dara", 100);
+    Mage mage = new Mage("Bob", 100);
+    Rogue rogue = new Rogue("Maxim", 100);
+
+    do{
+      System.out.println("A new fight begins...");
+
+      warrior.attack(rogue);
+      mage.castSpell(rogue);
+      rogue.attack(warrior);
+
+      warrior.levelUp();
+      mage.levelUp();
+      rogue.levelUp();
+
+      System.out.println("Do you want a new fight? (yes/no)");
+    }while (scanner.nextLine().equalsIgnoreCase("yes"));
+    scanner.close();
+    System.out.println("Game ended");
+ }
 }
