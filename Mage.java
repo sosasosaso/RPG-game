ef16912995f923b4cd525e9f496816d0d1c5fbbc
Dara Popova <86371720+sosasosaso@users.noreply.github.com>
2024-06-13 17:39:42 +0300

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -9,7 +9,20 @@ class Mage extends Character{
   }
   
 public void castSpell(Character target){
-  
+  if(getMana() >= 20){
+    setMana(getMana() - 20);
+    target.setHealth(getHealth() - 20);
+    System.out.println(getName() + " casted a spell on " + target.getName() + ". " + getName() + "'s mana is now: " + getMana());
+  }else{
+    System.out.println("Characer is not powerful enough to cast a spell");
+  }
 }
   
+public void levelUp(){
+   super.levelUp();
+        this.intelligence += 5;
+        this.spellPower += 5;
+    System.out.println(getName() + "'s intelligence is now: " + this.intelligence);
+    System.out.println(getName() + "'s spell power is now: " + this.spellPower);
+ }
 }
