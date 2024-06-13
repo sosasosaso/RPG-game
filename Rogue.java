6cc1002025652d2bd7d80667aba86760c207f934
Dara Popova <86371720+sosasosaso@users.noreply.github.com>
2024-06-13 17:46:54 +0300

    Update Rogue.java

--- Rogue.java
+++ Rogue.java
@@ -4,25 +4,26 @@ class Rogue extends Character{
 
   public Mage (String name, int health){
     super(name, health);
-    this.intelligence = 120;
-    this.spellPower = 200;
+    this.agility = 80;
+    this.dexterity = 100;
   }
   
-public void castSpell(Character target){
-  if(getMana() >= 20){
-    setMana(getMana() - 20);
-    target.setHealth(getHealth() - 20);
-    System.out.println(getName() + " casted a spell on " + target.getName() + ". " + getName() + "'s mana is now: " + getMana());
+public void attack(Character target){
+  if(getHealth() >= 10){
+    setHealth(getHealth() - 10);
+    target.setHealth(getHealth() - 10);
+    System.out.println(getName() + " attacked " + target.getName() + ". " + getName() + "'s health is now: " + getHealth());
+    System.out.println(target.getName() + "'s health is now: " + target.getHealth());
   }else{
-    System.out.println("Characer is not powerful enough to cast a spell");
+    System.out.println("Characer is not powerful enough to attack");
   }
 }
   
 public void levelUp(){
-   super.levelUp();
-        this.intelligence += 5;
-        this.spellPower += 5;
-    System.out.println(getName() + "'s intelligence is now: " + this.intelligence);
-    System.out.println(getName() + "'s spell power is now: " + this.spellPower);
+  super.levelUp();
+  this.agility += 5;
+  this.dexterity += 5;
+        System.out.println(getName() + "'s agility is now: " + this.agility);
+        System.out.println(getName() + "'s dexterity is now: " + this.dexterity);
  }
 }
